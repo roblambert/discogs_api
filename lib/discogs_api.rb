29d1dc9e65be48776ea2242a7bab13246b981d1f
Rob Lambert <rob@zabada.com>
2011-07-11 17:16:31 -0500

    changed DiscogsApi to a module and make all methods class level methods

--- lib/discogs_api.rb
+++ lib/discogs_api.rb
@@ -4,64 +4,62 @@ require 'open-uri'
 #
 # This plugin simply retrieves JSON from the Discogs API and wraps the response in a Hashie::Mash
 # to return pseudo-objects that have method-like accessors.
-class DiscogsApi
-
-  attr_accessor :user_agent
+module DiscogsApi
 
   BASE_URL = 'http://api.discogs.com'
 
-  def initialize(user_agent = "YourUserAgentForDiscogsAPI2/YourVersion +http://youraddress")
-    @user_agent = user_agent
+  def self.user_agent=(value)
+    @@user_agent = value
   end
 
-  def get_master(id)
+  def self.get_master(id)
     get_resource('master', id)
   end
 
-  def get_release(id)
+  def self.get_release(id)
     get_resource('release', id)
   end
 
-  def search_releases(criteria, page = 1)
+  def self.search_releases(criteria, page = 1)
     search(criteria, 'releases', page)
   end
 
-  def get_artist(artist_name)
+  def self.get_artist(artist_name)
     get_resource('artist', artist_name, {'releases'=>'1'}) #include artist releases per http://www.discogs.com/help/forums/topic/234163
   end
 
-  def search_artists(criteria, page = 1)
+  def self.search_artists(criteria, page = 1)
     search(criteria, 'artists')
   end
 
-  def get_label(label_name)
+  def self.get_label(label_name)
     get_resource('label', label_name)
   end
 
-  def search_labels(criteria, page = 1)
+  def self.search_labels(criteria, page = 1)
     search(criteria, 'labels')
   end
   
-  def resource_url(resource, id, params = {})
+  def self.resource_url(resource, id, params = {})
     "#{BASE_URL}/#{resource}/#{CGI.escape(id.to_s)}?f=json#{params.keys.sort.inject(""){|string,key| "#{string}&#{key}=#{CGI.escape(params[key].to_s)}"}}"
   end
   
   private
 
-  def get_resource(resource, id, params = {})
+  def self.get_resource(resource, id, params = {})
     retrieve_data(resource, resource_url(resource, id, params))
   end
   
-  def retrieve_data(response_type, url)
+  def self.retrieve_data(response_type, url)
     #TODO: deal w/ gzipped content
-    JSON.parse(open(url, {'UserAgent'=> @user_agent,'Accept-Encoding'=>'gzip'}).read)["resp"][response_type]     
+    JSON.parse(open(url, {'UserAgent'=> @@user_agent,'Accept-Encoding'=>'gzip'}).read)["resp"][response_type]     
   end
   
-  def search(criteria, resource_type = 'all', page = 1)
+  def self.search(criteria, resource_type = 'all', page = 1)
     retrieve_data('search', search_url(criteria, resource_type))["searchresults"]
   end
   
-  def search_url(criteria, resource_type, page = 1 )
+  def self.search_url(criteria, resource_type, page = 1 )
     "#{BASE_URL}/search?type=#{resource_type}&f=json&page=#{page}&q=#{CGI.escape(criteria)}"
   end
 
--- spec/discogs_api_spec.rb
+++ spec/discogs_api_spec.rb
@@ -1,14 +1,14 @@
 require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
 
 module DiscogsApiSpecHelper
-  def mock_resource(api, resource, id, params = {})
+  def mock_resource(resource, id, params = {})
     content = File.open(RAILS_ROOT + "/vendor/plugins/discogs_api/spec/mock-data/discogs-#{resource}-#{id}.json")
-    api.should_receive(:open).exactly(1).times.with(any_args()).and_return(content)
+    DiscogsApi.should_receive(:open).exactly(1).times.with(any_args()).and_return(content)
   end
 
-  def mock_search(api, response_type, criteria, page = 1)
+  def mock_search(response_type, criteria, page = 1)
     content = File.open(RAILS_ROOT + "/vendor/plugins/discogs_api/spec/mock-data/discogs-search-#{response_type}-#{criteria}-#{page}.json")
-    api.should_receive(:open).exactly(1).times.with(any_args()).and_return(content)
+    DiscogsApi.should_receive(:open).exactly(1).times.with(any_args()).and_return(content)
   end
 end
 
@@ -16,32 +16,28 @@ describe DiscogsApi do
   include DiscogsApiSpecHelper
 
   before(:each) do
-    @api = DiscogsApi.new 'TestRubyClient/0.1 +http://testwebsite'
-  end
-  
-  it "should use the provided constructor arg as the user_agent" do
-    @api.user_agent.should == 'TestRubyClient/0.1 +http://testwebsite'
+    DiscogsApi.user_agent = 'TestRubyClient/0.1 +http://testwebsite'
   end
 
   it "should use properly encoded URLs" do
-    @api.resource_url('release', '1234').should == 'http://api.discogs.com/release/1234?f=json'
-    @api.resource_url('artist', 'Van Halen').should == 'http://api.discogs.com/artist/Van+Halen?f=json'
-    @api.resource_url('artist', 'Van Halen', {'releases', '1'}).should == 'http://api.discogs.com/artist/Van+Halen?f=json&releases=1'
+    DiscogsApi.resource_url('release', '1234').should == 'http://api.discogs.com/release/1234?f=json'
+    DiscogsApi.resource_url('artist', 'Van Halen').should == 'http://api.discogs.com/artist/Van+Halen?f=json'
+    DiscogsApi.resource_url('artist', 'Van Halen', {'releases', '1'}).should == 'http://api.discogs.com/artist/Van+Halen?f=json&releases=1'
   end
 
   it "should retrieve a master release" do
-    mock_resource(@api, 'master', '45526')
+    mock_resource('master', '45526')
 
-    master = Hashie::Mash.new @api.get_master('45526')
+    master = Hashie::Mash.new DiscogsApi.get_master('45526')
     master.id.should == 45526
     master.main_release.should == 375379
     master.artists.first.name.should == "Beatles, The"
   end
 
   it "should retrieve a release" do
-    mock_resource(@api, 'release', '375379')
+    mock_resource('release', '375379')
 
-    release = Hashie::Mash.new @api.get_release('375379')
+    release = Hashie::Mash.new DiscogsApi.get_release('375379')
     release.id.should == 375379
     release.title.should == 'Rubber Soul'
     release.artists.first.name.should == "Beatles, The"
@@ -49,17 +45,17 @@ describe DiscogsApi do
   end
 
   it "should retrieve an Artist" do
-    mock_resource(@api, 'artist', 'Van Halen')
+    mock_resource( 'artist', 'Van Halen')
 
-    artist = Hashie::Mash.new @api.get_artist('Van Halen')
+    artist = Hashie::Mash.new DiscogsApi.get_artist('Van Halen')
     artist.name.should == 'Van Halen'
     artist.urls.first.should == "http://www.van-halen.com"
   end
 
   it "should retrieve a label" do
-    mock_resource(@api, 'label', 'Sire')
+    mock_resource( 'label', 'Sire')
 
-    label = Hashie::Mash.new @api.get_label('Sire')
+    label = Hashie::Mash.new DiscogsApi.get_label('Sire')
     label.name.should == 'Sire'
     label.parentLabel.should == 'Warner Bros. Records'
     label.urls.first.should == 'http://www.sirerecords.com/'
@@ -67,9 +63,9 @@ describe DiscogsApi do
   end
 
   it "should search for releases first page when not specified" do
-    mock_search(@api, 'releases', 'van')
+    mock_search( 'releases', 'van')
 
-    releases = Hashie::Mash.new @api.search_releases('van')
+    releases = Hashie::Mash.new DiscogsApi.search_releases('van')
     releases.numResults.should == '83377'
     releases.start.should == '1'
     releases.results.first.title.should == 'Van - Ludwig Van'
@@ -77,9 +73,9 @@ describe DiscogsApi do
   end
 
   it "should search for releases specified page" do
-    mock_search(@api, 'releases', 'van', 2)
+    mock_search( 'releases', 'van', 2)
 
-    releases = Hashie::Mash.new @api.search_releases('van', 2)
+    releases = Hashie::Mash.new DiscogsApi.search_releases('van', 2)
     releases.numResults.should == '83377'
     releases.start.should == '21'
     releases.results.first.title.should == 'Alias (6) - Can I / Van Cleef'
@@ -87,9 +83,9 @@ describe DiscogsApi do
   end
 
   it "should search for artists first page" do
-    mock_search(@api, 'artists', 'van')
+    mock_search( 'artists', 'van')
 
-    artists = Hashie::Mash.new @api.search_artists('van')
+    artists = Hashie::Mash.new DiscogsApi.search_artists('van')
     artists.numResults.should == '5521'
     artists.start.should == '1'
     artists.results.first.title.should == 'Van Cephus'
@@ -97,9 +93,9 @@ describe DiscogsApi do
   end
 
   it "should search for artists specified page" do
-    mock_search(@api, 'artists', 'van', 2)
+    mock_search( 'artists', 'van', 2)
 
-    artists = Hashie::Mash.new @api.search_artists('van', 2)
+    artists = Hashie::Mash.new DiscogsApi.search_artists('van', 2)
     artists.numResults.should == '5521'
     artists.start.should == '21'
     artists.results.first.title.should == 'van Andel'
@@ -107,18 +103,18 @@ describe DiscogsApi do
   end
 
   it "should search for labels first page" do
-    mock_search(@api, 'labels', 'van')
+    mock_search( 'labels', 'van')
 
-    labels = Hashie::Mash.new @api.search_labels('van')
+    labels = Hashie::Mash.new DiscogsApi.search_labels('van')
     labels.numResults.should == '205'
     labels.start.should == '1'
     labels.results.first.title.should == 'Van Record Company'
   end
 
   it "should search for labels specified page" do
-    mock_search(@api, 'labels', 'van', 2)
+    mock_search( 'labels', 'van', 2)
 
-    labels = Hashie::Mash.new @api.search_artists('van', 2)
+    labels = Hashie::Mash.new DiscogsApi.search_artists('van', 2)
     labels.numResults.should == '205'
     labels.start.should == '21'
     labels.results.first.title.should == 'Funny Vinyl'
